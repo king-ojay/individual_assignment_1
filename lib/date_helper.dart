82616c8bcbfa668a628a41d21594cb55cb720413
Your Name <you@example.com>
2025-09-20 21:18:32 +0200

    done

--- lib/date_helper.dart
+++ lib/date_helper.dart
@@ -1,11 +1,11 @@
-ximport 'package:intl/intl.dart';
+import 'package:intl/intl.dart';
 
 class DateHelper {
   // Check if two dates are the same day
   static bool isSameDay(DateTime date1, DateTime date2) {
-    return date1.year == date2.year && 
-           date1.month == date2.month && 
-           date1.day == date2.day;
+    return date1.year == date2.year &&
+        date1.month == date2.month &&
+        date1.day == date2.day;
   }
 
   // Format date nicely
