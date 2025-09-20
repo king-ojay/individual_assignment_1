0d37220fe3a957780cedc37a025c0d9fc0caf087
Your Name <you@example.com>
2025-09-20 21:52:57 +0200

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'task.dart';
-import 'storage_service.dart';
-import 'date_helper.dart';
+import 'today_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,63 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Study Planner Test', home: TestScreen());
-  }
-}
-
-class TestScreen extends StatefulWidget {
-  @override
-  _TestScreenState createState() => _TestScreenState();
-}
-
-class _TestScreenState extends State<TestScreen> {
-  List<Task> tasks = [];
-
-  @override
-  void initState() {
-    super.initState();
-    _loadTasks();
-  }
-
-  Future<void> _loadTasks() async {
-    final loadedTasks = await StorageService.loadTasks();
-    setState(() {
-      tasks = loadedTasks;
-    });
-  }
-
-  Future<void> _addTestTask() async {
-    final newTask = Task(
-      title: 'Test Task ${tasks.length + 1}',
-      description: 'This is a test',
-      dueDate: DateTime.now(),
-    );
-
-    tasks.add(newTask);
-    await StorageService.saveTasks(tasks);
-    setState(() {});
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Storage Test'), backgroundColor: Colors.blue),
-      body: tasks.isEmpty
-          ? Center(child: Text('No tasks. Tap + to test storage!'))
-          : ListView.builder(
-              itemCount: tasks.length,
-              itemBuilder: (context, index) {
-                final task = tasks[index];
-                return ListTile(
-                  title: Text(task.title),
-                  subtitle: Text(DateHelper.formatDate(task.dueDate)),
-                );
-              },
-            ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _addTestTask,
-        child: Icon(Icons.add),
-      ),
-    );
+    return MaterialApp(title: 'Study Planner', home: TodayScreen());
   }
 }
